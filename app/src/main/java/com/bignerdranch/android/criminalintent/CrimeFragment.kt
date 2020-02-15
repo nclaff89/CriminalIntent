4dc7f4031d0c631fff029d47437e041cc02acf8d
Nick <nicholas.claffey@outlook.com>
2020-02-14 18:36:09 -0600

    completed chapter 18

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
@@ -217,8 +217,10 @@ class CrimeFragment :Fragment(), DatePickerFragment.Callbacks {
         if(photoFile.exists()){
             val bitmap = getScaledBitmap(photoFile.path, requireActivity())
             photoView.setImageBitmap(bitmap)
+            photoView.contentDescription = getString(R.string.crime_photo_image_description)
         }else{
             photoView.setImageDrawable(null)
+            photoView.contentDescription = getString(R.string.crime_photo_no_image_description)
         }
     }
 
